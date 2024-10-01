baf93aecc8bd1e5ff302964cbf99db353e1fd82c
jaya-git-123 <jayamani46059@gmail.com>
2024-10-01 23:11:52 +0530

    updated code

--- src/App.js
+++ src/App.js
@@ -1,17 +1,11 @@
 import React from 'react';
 import './App.css';
-// import Navbar from './components/Navbar';
 import Header from './components/Header';
-// import Home from './components/Home';
-// import About from './components/About';
 
 function App() {
   return (
     <div className="App">
       <Header />
-      {/* <Navbar /> */}
-      {/* <Home /> */}
-      {/* <About /> */}
     </div>
   );
 }
